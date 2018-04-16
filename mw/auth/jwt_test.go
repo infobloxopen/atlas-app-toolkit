0571f247c010d3fc800d0aec394657fed7fa515f
Zachary Eddy <zeddy@infoblox.com>
2018-04-16 14:20:34 -0700

    Change how multitenant ID is extracted from JWT

--- mw/auth/jwt_test.go
+++ mw/auth/jwt_test.go
@@ -1,39 +1,83 @@
 package auth
 
 import (
+	"context"
+	"fmt"
 	"testing"
 
 	jwt "github.com/dgrijalva/jwt-go"
+	"google.golang.org/grpc/metadata"
+)
+
+const (
+	TEST_SECRET = "some-secret-123"
 )
 
 func TestGetTenantID(t *testing.T) {
 	var tenantIDTests = []struct {
-		claims   jwt.Claims
+		claims   jwt.MapClaims
+		field    string
 		expected string
 		err      error
 	}{
 		{
-			jwt.MapClaims{
-				"TenantID": "tenantid-abc-123",
+			claims: jwt.MapClaims{
+				"some-field": "id-abc-123",
 			},
-			"tenantid-abc-123",
-			nil,
+			field:    "some-field",
+			expected: "id-abc-123",
+			err:      nil,
 		},
 		{
-			jwt.MapClaims{},
-			"",
-			errMissingTenantID,
+			claims: jwt.MapClaims{
+				"some-field": "id-abc-123",
+			},
+			field:    "some-other-field",
+			expected: "",
+			err:      errMissingField,
+		},
+		{
+			claims:   jwt.MapClaims{},
+			field:    "some-field",
+			expected: "",
+			err:      errMissingToken,
 		},
 	}
 	for _, test := range tenantIDTests {
-		token := makeToken(test.claims, t)
-		ctx, err := contextWithToken(token)
-		tenantID, err := GetTenantID(ctx, nil)
+		ctx := context.Background()
+		if len(test.claims) != 0 {
+			token := makeToken(test.claims, t)
+			ctx, _ = contextWithToken(token)
+		}
+		actual, err := GetJWTField(ctx, test.field, nil)
 		if err != test.err {
 			t.Errorf("Invalid error value: %v - expected %v", err, test.err)
 		}
-		if tenantID != test.expected {
-			t.Errorf("Invalid tenant ID: %v - expected %v", tenantID, test.expected)
+		if actual != test.expected {
+			t.Errorf("Invalid tenant ID: %v - expected %v", actual, test.expected)
 		}
 	}
+}
+
+// creates a context with a jwt
+func contextWithToken(token string) (context.Context, error) {
+	md := metadata.Pairs(
+		"authorization", fmt.Sprintf("token %s", token),
+	)
+	return metadata.NewIncomingContext(context.Background(), md), nil
+}
+
+// generates a token string based on the given jwt claims
+func makeToken(claims jwt.Claims, t *testing.T) string {
+	method := jwt.SigningMethodHS256
+	token := jwt.NewWithClaims(method, claims)
+	signingString, err := token.SigningString()
+	if err != nil {
+		t.Fatalf("Error when building token: %v", err)
+	}
+	signature, err := method.Sign(signingString, []byte(TEST_SECRET))
+	if err != nil {
+		t.Fatalf("Error when building token: %v", err)
+	}
+	return fmt.Sprintf("%s.%s", signingString, signature)
 }
