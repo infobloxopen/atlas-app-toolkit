06ca2893e8cfdab5895bfdd19f249989ad7f3c9f
Mayank Kumar <makumar@infoblox.com>
2018-05-31 16:00:18 +0530

    Add Request-Id middleware (#45)
    * Add request id middleware
    
      - Add interceptor to extract Request-Id from incoming metadata and update
        context and outgoing metadata with the same.
      - Create new Request-Id in cases when incoming metadata does not contain
        Request-Id
      - Add unit tests
    
    * Add Request-Id middleware
    
      - Fix FromContext signature
    
    * Add Request-Id middleware
    
      - fix goimports
      - fix golint
      - fix review comments
    
    * Add Request-Id middleware
    
    * Add Request-Id middleware

---
+++ requestid/interceptor.go
@@ -0,0 +1,39 @@
+package requestid
+
+import (
+	"context"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/grpclog"
+	"google.golang.org/grpc/status"
+)
+
+// UnaryServerInterceptor returns grpc.UnaryServerInterceptor
+// that should be used as a middleware to generate/include Request-Id in headers and context
+// for tracing and tracking user's request.
+//
+//
+// Returned middleware populates Request-Id from gRPC metadata if
+// they defined in a testRequest message else creates a new one.
+func UnaryServerInterceptor() grpc.UnaryServerInterceptor {
+	return func(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (res interface{}, err error) {
+		// handle panic
+		defer func() {
+			if perr := recover(); perr != nil {
+				err = status.Errorf(codes.Internal, "request id interceptor: %s", perr)
+				grpclog.Errorln(err)
+				res, err = nil, err
+			}
+		}()
+
+		reqID := handleRequestID(ctx)
+
+		ctx = NewContext(ctx, reqID)
+
+		// returning from the request call
+		res, err = handler(ctx, req)
+
+		return
+	}
+}
---
+++ requestid/requestid.go
@@ -0,0 +1,43 @@
+package requestid
+
+import (
+	"context"
+
+	"github.com/google/uuid"
+	"github.com/infobloxopen/atlas-app-toolkit/gateway"
+	"google.golang.org/grpc/metadata"
+)
+
+// DefaultRequestIDKey is the metadata key name for request ID
+const DefaultRequestIDKey = "Request-Id"
+
+func handleRequestID(ctx context.Context) (reqID string) {
+	reqID, exists := FromContext(ctx)
+	if !exists {
+		reqID := newRequestID()
+		return reqID
+	}
+
+	if reqID == "" {
+		reqID := newRequestID()
+		return reqID
+	}
+
+	return reqID
+}
+
+func newRequestID() string {
+	return uuid.New().String()
+}
+
+// FromContext returns the Request-Id information from ctx if it exists.
+func FromContext(ctx context.Context) (reqID string, exists bool) {
+	reqID, exists = gateway.Header(ctx, DefaultRequestIDKey)
+	return
+}
+
+// NewContext creates a new context with Request-Id attached if not exists.
+func NewContext(ctx context.Context, reqID string) context.Context {
+	md := metadata.Pairs(DefaultRequestIDKey, reqID)
+	return metadata.NewOutgoingContext(ctx, md)
+}
