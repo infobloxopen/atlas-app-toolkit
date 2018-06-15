57d83ea8b39cd55f2bb79c1600652086118834eb
Andrei Maskalenka <amaskalenka@infoblox.com>
2018-06-15 16:07:00 +0300

    Implementation of jsonpb.JSONPBMarshale and jsonpb.JSONPBUnmarshaler interfaces. UTs

--- rpc/resource/resourcepb/jsonpb.go
+++ rpc/resource/resourcepb/jsonpb.go
@@ -1,16 +1,34 @@
 package resourcepb
 
 import (
+	"strings"
+
 	"github.com/golang/protobuf/jsonpb"
 
 	"github.com/infobloxopen/atlas-app-toolkit/rpc/resource/internal"
 )
 
+// MarshalJSONPB implements jsonpb.JSONPBMarshaler interface by marshal
+// Identifier from a JSON string in accordance with Atlas Reference format
+// 		<application_name>/<resource_type>/<resource_id>
+// Support "null" value.
 func (m Identifier) MarshalJSONPB(*jsonpb.Marshaler) ([]byte, error) {
-	return []byte(internal.BuildString(m.ApplicationName, m.ResourceType, m.ResourceId)), nil
+	v := internal.BuildString(m.ApplicationName, m.ResourceType, m.ResourceId)
+	if v == "" {
+		v = "null"
+	}
+	return []byte(v), nil
 }
 
-func (m *Identifier) UnmarhsalJSONPB(_ *jsonpb.Unmarshaler, data []byte) error {
-	m.ApplicationName, m.ResourceType, m.ResourceId = internal.ParseString(string(data))
+// UnmarshalJSONPB implements jsonpb.JSONPBUnmarshaler interface by unmarshal
+// Identifier to a JSON string in accordance with Atlas Reference format
+// 		<application_name>/<resource_type>/<resource_id>
+// Support "null" value.
+func (m *Identifier) UnmarshalJSONPB(_ *jsonpb.Unmarshaler, data []byte) error {
+	v := strings.Trim(string(data), "\"")
+	if v == "null" {
+		v = ""
+	}
+	m.ApplicationName, m.ResourceType, m.ResourceId = internal.ParseString(v)
 	return nil
 }
