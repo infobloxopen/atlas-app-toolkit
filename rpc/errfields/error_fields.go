be7760a7abe4598cef134f0b73765d8196ede847
aliaksandr_skurydzin <askurydzin@localhost.localdomain>
2018-06-20 14:32:35 +0300

    gateway fields handling implementation.

--- rpc/errfields/error_fields.go
+++ rpc/errfields/error_fields.go
@@ -1,6 +1,8 @@
 package errfields
 
-import ()
+import (
+	"encoding/json"
+)
 
 func (m *FieldInfo) AddField(target string, msg string) {
 	if m.Fields == nil {
@@ -13,3 +15,39 @@ func (m *FieldInfo) AddField(target string, msg string) {
 
 	m.Fields[target].Values = append(m.Fields[target].Values, msg)
 }
+
+// MarshalJSON implements json.Marshaler.
+func (fi *FieldInfo) MarshalJSON() ([]byte, error) {
+	fiMap := map[string][]string{}
+
+	for k, v := range fi.Fields {
+		var descArr []string
+		if v != nil {
+			descArr = make([]string, len(v.Values))
+			for i, desc := range v.Values {
+				descArr[i] = desc
+			}
+		}
+
+		fiMap[k] = descArr
+	}
+
+	return json.Marshal(&fiMap)
+}
+
+// UnmarshalJSON implements json.Unmarshaler.
+func (fi *FieldInfo) UnmarshalJSON(data []byte) error {
+	dst := map[string][]string{}
+	if err := json.Unmarshal(data, &dst); err != nil {
+		return err
+	}
+
+	for k, v := range dst {
+		for _, vVal := range v {
+			fi.AddField(k, vVal)
+		}
+	}
+
+	return nil
+}
+
