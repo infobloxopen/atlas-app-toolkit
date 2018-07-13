cbec16e6992caed875566d7ab2290877920751e9
Zachary Eddy <zeddy@infoblox.com>
2018-07-13 14:06:22 -0700

    Add helpers to support integration testing with a Postgres database

---
+++ integration/postgres.go
@@ -0,0 +1,198 @@
+package integration
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+	"time"
+)
+
+var (
+	errConnectionTimeout = testDatabaseError{
+		message: "database connection timed out",
+	}
+)
+
+type testDatabaseError struct{ message string }
+
+func (e testDatabaseError) Error() string {
+	return fmt.Sprintf("testing database error: %s", e.message)
+}
+
+type testPostgresDB struct {
+	host            string
+	port            int
+	dbName          string
+	dbUser          string
+	dbPassword      string
+	dbVersion       string
+	migrateFunction func(sql.DB) error
+	timeout         time.Duration
+}
+
+// NewTestPostgresDB returns a test postgres database that
+// the functional options that have been provided by the caller
+func NewTestPostgresDB(opts ...option) (testPostgresDB, error) {
+	port, err := GetOpenPortInRange(35000, portRangeMax)
+	if err != nil {
+		return testPostgresDB{}, err
+	}
+	db := testPostgresDB{
+		port:       port,
+		dbName:     "test-postgres-db",
+		dbUser:     "postgres",
+		dbPassword: "postgres",
+		dbVersion:  "latest",
+		timeout:    time.Second * 10,
+	}
+	for _, opt := range opts {
+		opt(&db)
+	}
+	return db, nil
+}
+
+// Reset drops all the tables in a test database and regenerates them by
+// running migrations. If a migration function has not been specified, then the
+// tables are dropped but not regenerated
+func (db testPostgresDB) Reset() error {
+	dbSQL, err := sql.Open("postgres", db.GetDSN())
+	if err != nil {
+		return err
+	}
+	defer dbSQL.Close()
+	resetQuery := "DROP SCHEMA public CASCADE;" +
+		"CREATE SCHEMA public;" +
+		"GRANT ALL ON SCHEMA public TO postgres;" +
+		"GRANT ALL ON SCHEMA public TO public;"
+	// drop all the tables in the test database
+	if _, err := dbSQL.Exec(resetQuery); err != nil {
+		return err
+	}
+	// run migrations if a migration function has exists
+	if db.migrateFunction != nil {
+		if err := db.migrateFunction(*dbSQL); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+// RunAsDockerContainer spins-up a Postgres database server as a Docker
+// container. The test Postgres database will run inside this Docker container.
+func (db testPostgresDB) RunAsDockerContainer(containerName string) (func() error, error) {
+	cleanup, err := RunContainer(
+		// define the postgres image version
+		fmt.Sprintf("postgres:%s", db.dbVersion),
+		// define the arguments to docker
+		[]string{
+			fmt.Sprintf("--name=%s", containerName),
+			fmt.Sprintf("--publish=%d:5432", db.port),
+			fmt.Sprintf("--env=POSTGRES_DB=%s", db.dbName),
+			fmt.Sprintf("--env=POSTGRES_PASSWORD=%s", db.dbPassword),
+			fmt.Sprintf("--env=POSTGRES_USER=%s", db.dbUser),
+			"--detach",
+			"--rm",
+		},
+		// define the runtime arguments to postgres
+		[]string{},
+	)
+	if err != nil {
+		return nil, err
+	}
+	if err := db.checkConnection(); err != nil {
+		cleanup()
+		return nil, err
+	}
+	return cleanup, nil
+}
+
+func (db testPostgresDB) checkConnection() error {
+	ctx, cancel := context.WithTimeout(context.Background(), db.timeout)
+	defer cancel()
+	errStream := make(chan error)
+	go func() {
+		for {
+			select {
+			case <-time.After(500 * time.Millisecond):
+				driver, _ := sql.Open("postgres", db.GetDSN())
+				if err := driver.Ping(); err == nil {
+					errStream <- nil
+					return
+				}
+			case <-ctx.Done():
+				errStream <- errConnectionTimeout
+			}
+		}
+	}()
+	if err := <-errStream; err != nil {
+		return err
+	}
+	return nil
+}
+
+// GetDSN returns the database connection string for the test Postgres database
+func (db testPostgresDB) GetDSN() string {
+	return fmt.Sprintf(
+		"host=localhost port=%d user=%s password=%s sslmode=disable dbname=%s",
+		db.port, db.dbUser, db.dbPassword, db.dbName,
+	)
+}
+
+type option func(*testPostgresDB)
+
+// WithName is used to specify the name of the test Postgres database. By default
+// the database name is "test-postgres-db"
+func WithName(name string) func(*testPostgresDB) {
+	return func(db *testPostgresDB) {
+		db.dbName = name
+	}
+}
+
+// WithPort is used to specify the port of the test Postgres database. By
+// default, the test database will find the first open port in the 35000+ range
+func WithPort(port int) func(*testPostgresDB) {
+	return func(db *testPostgresDB) {
+		db.port = port
+	}
+}
+
+// WithUser is used to specify the name of the Postgres user that owns the
+// test database
+func WithUser(user string) func(*testPostgresDB) {
+	return func(db *testPostgresDB) {
+		db.dbUser = user
+	}
+}
+
+// WithPassword is used to specify the password of the test Postgres database
+func WithPassword(password string) func(*testPostgresDB) {
+	return func(db *testPostgresDB) {
+		db.dbPassword = password
+	}
+}
+
+// WithVersion is used to specify the version of the test Postgres database. By
+// default the version is "latest"
+func WithVersion(version string) func(*testPostgresDB) {
+	return func(db *testPostgresDB) {
+		db.dbVersion = version
+	}
+}
+
+// WithMigrateFunction is used to rebuild the test Postgres database on a
+// per-test basis. Whenever the database is reset with the Reset() function, the
+// migrate function will rebuild the tables.
+func WithMigrateFunction(migrateFunction func(sql.DB) error) func(*testPostgresDB) {
+	return func(db *testPostgresDB) {
+		db.migrateFunction = migrateFunction
+	}
+}
+
+// WithMigrateFunction is used to rebuild the test Postgres database on a
+// per-test basis. Whenever the database is reset with the Reset() function, the
+// migrate function will rebuild the tables.
+func WithTimeout(timeout time.Duration) func(*testPostgresDB) {
+	return func(db *testPostgresDB) {
+		db.timeout = timeout
+	}
+}
---
+++ integration/postgres_test.go
@@ -0,0 +1,120 @@
+package integration
+
+import (
+	"database/sql"
+	"errors"
+	"testing"
+	"time"
+
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
+)
+
+// buildDB creates a new test Postgres database and halts the test if anything
+// fails during this process
+func buildDB(t *testing.T, opts ...option) testPostgresDB {
+	db, err := NewTestPostgresDB(opts...)
+	if err != nil {
+		t.Fatalf("unable to create test postgres database")
+	}
+	return db
+}
+
+func TestGetDSN(t *testing.T) {
+	var tests = []struct {
+		name     string
+		opts     []option
+		expected string
+	}{
+		{
+			name: "vanilla test database",
+			opts: []option{
+				// test will fail if default port is taken, so the port is specified
+				WithPort(37000),
+			},
+			expected: "host=localhost port=37000 user=postgres " +
+				"password=postgres sslmode=disable dbname=test-postgres-db",
+		},
+		{
+			name: "test with all options",
+			opts: []option{
+				WithName("some-test-db"),
+				WithPort(37000),
+				WithUser("test-user"),
+				WithPassword("secret"),
+				WithVersion("9.5.13"),
+			},
+			expected: "host=localhost port=37000 user=test-user " +
+				"password=secret sslmode=disable dbname=some-test-db",
+		},
+	}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			db, err := NewTestPostgresDB(test.opts...)
+			if err != nil {
+				t.Fatalf("unable to build test database")
+			}
+			if dsn := db.GetDSN(); dsn != test.expected {
+				t.Errorf("unexpected database connection string: have %s; want %s",
+					dsn, test.expected,
+				)
+			}
+		})
+	}
+}
+
+func TestCheckConnection(t *testing.T) {
+	db, err := NewTestPostgresDB(
+		WithTimeout(time.Second * 5),
+	)
+	if err != nil {
+		t.Fatalf("unexpected error: %v", err)
+	}
+	db.host = "some-fake-hostname"
+	if err := db.checkConnection(); err == nil {
+		t.Errorf("expected to get non-nil error")
+	}
+	db.host = "localhost"
+	rm, err := db.RunAsDockerContainer("test-postgres-container")
+	if err != nil {
+		t.Fatalf("unable to start the database container: %v", err)
+	}
+	defer rm()
+}
+
+type testTable struct {
+	gorm.Model
+	Test string
+}
+
+func TestReset(t *testing.T) {
+	db, err := NewTestPostgresDB()
+	if err != nil {
+		t.Fatalf("unexpected error: %v", err)
+	}
+	if err := db.Reset(); err == nil {
+		t.Errorf("expected to receive an error when resetting database")
+	}
+	rm, err := db.RunAsDockerContainer("test-postgres-container")
+	if err != nil {
+		t.Fatalf("unable to start the database container: %v", err)
+	}
+	defer rm()
+	db.migrateFunction = func(sql.DB) error {
+		return errors.New("intentional testing error")
+	}
+	if err := db.Reset(); err == nil {
+		t.Errorf("expected to receive an error when migrating database")
+	}
+	db.migrateFunction = func(sql.DB) error {
+		orm, err := gorm.Open("postgres", db.GetDSN())
+		if err != nil {
+			t.Errorf("unable to connect to database: %v", err)
+		}
+		orm.AutoMigrate(&testTable{})
+		return nil
+	}
+	if err := db.Reset(); err != nil {
+		t.Errorf("unexpected error when resetting the database: %v", err)
+	}
+}
