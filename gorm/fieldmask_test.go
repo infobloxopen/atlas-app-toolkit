1240433dcebfa52c9c3a58cc924f53b16d8b6502
Caleb Horst <calebjh1@gmail.com>
2018-07-16 10:58:38 -0700

    Add function to copy masked fields between two objects (#58)
    * Add function to copy masked fields between two objects
    
    * Rename gorm/field.go -> gorm/fieldmask.go because collision

---
+++ gorm/fieldmask_test.go
@@ -0,0 +1,52 @@
+package gorm
+
+import (
+	"errors"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"google.golang.org/genproto/protobuf/field_mask"
+)
+
+type childTest struct {
+	FieldOne   int
+	FieldTwo   string
+	FieldThree *int
+	FieldFour  []int
+}
+
+type topTest struct {
+	FieldA childTest
+	FieldB *childTest
+}
+
+func wrapInt(x int) *int {
+	return &x
+}
+
+func TestMergeWithMask(t *testing.T) {
+	source := &topTest{
+		FieldA: childTest{FieldOne: 22, FieldTwo: "catch", FieldThree: wrapInt(2), FieldFour: []int{1, 2, 3}},
+		FieldB: &childTest{FieldOne: 3, FieldTwo: "string", FieldThree: wrapInt(1), FieldFour: []int{3, 2, 1}},
+	}
+	dest := &topTest{}
+	err := MergeWithMask(source, dest, &field_mask.FieldMask{Paths: []string{"FieldB.FieldOne", "FieldA.FieldTwo", "FieldA.FieldThree", "FieldB.FieldFour"}})
+	assert.Equal(t, &topTest{
+		FieldA: childTest{FieldTwo: "catch", FieldThree: wrapInt(2)},
+		FieldB: &childTest{FieldOne: 3, FieldFour: []int{3, 2, 1}},
+	}, dest)
+	assert.Nil(t, err)
+
+	err = MergeWithMask(source, dest, &field_mask.FieldMask{Paths: []string{"FieldB.FieldDNE", "FieldA.FieldTwo"}})
+	assert.Equal(t, errors.New("Field path \"FieldB.FieldDNE\" doesn't exist in type *gorm.topTest"), err)
+
+	err = MergeWithMask(nil, nil, nil)
+	assert.Equal(t, errors.New("Source object is nil"), err)
+	err = MergeWithMask(source, nil, nil)
+	assert.Equal(t, errors.New("Destination object is nil"), err)
+	err = MergeWithMask(source, dest, nil)
+	assert.Equal(t, errors.New("FieldMask is nil"), err)
+	err = MergeWithMask(source, dest.FieldA, &field_mask.FieldMask{})
+	assert.Equal(t, errors.New("Types of source and destination objects do not match"), err)
+}
