cc7ebeec8ebf09c585a398708272ec68c52e6c06
Evgeniy-L <evgeniyl@infoblox.com>
2018-08-10 16:46:13 +0300

    Move CamelToSnake to util package

--- errors/mappers/validationerrors/interceptor_test.go
+++ errors/mappers/validationerrors/interceptor_test.go
@@ -169,55 +169,6 @@ func TestUnaryServerInterceptor_Success(t *testing.T) {
 	}
 }
 
-// TestUnaryServerInterceptor_ValidationErrors will run mock validation errors to see if it parses correctly.
-func TestCamelToSnake(t *testing.T) {
-	tests := []struct {
-		name     string
-		actual   string
-		expected string
-	}{
-		// Test cases
-		{
-			"Testing CamelCase",
-			"CamelCase",
-			"camel_case",
-		},
-		{
-			"Testing AnotherCamel123",
-			"AnotherCamel123",
-			"another_camel123",
-		},
-		{
-			"Testing testCase",
-			"testCase",
-			"test_case",
-		},
-		{
-			"Testing testcase",
-			"testcase",
-			"testcase",
-		},
-		{
-			"Testing JSONData",
-			"TestCaseUUID",
-			"test_case_uuid",
-		},
-		{
-			"Testing JSONData",
-			"JSONData",
-			"json_data",
-		},
-	}
-	for _, tt := range tests {
-		actual := CamelToSnake(tt.actual)
-		expected := tt.expected
-		if actual != expected {
-			t.Errorf("CamelToSnake failed for test %s, expected: \"%s\", actual: \"%s\"", tt.name, expected, actual)
-		}
-
-	}
-}
-
 // testResponse represents a mock response.
 type testResponse struct{}
 
