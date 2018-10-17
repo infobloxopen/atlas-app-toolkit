af356f9e74199abfd3afc9e2fbaf316edffdad1f
chorst <chorst@infoblox.com>
2018-10-17 15:09:56 -0700

    Add per-request context-logger level and pseudo-tracing tags

---
+++ logging/annotator.go
@@ -0,0 +1,34 @@
+package logging
+
+import (
+	"context"
+	"net/http"
+
+	"github.com/grpc-ecosystem/grpc-gateway/runtime"
+	"google.golang.org/grpc/metadata"
+)
+
+// HTTP Headers
+const logLevelHeaderKey = "log-level"
+const logFlagHeaderKey = "log-trace-key"
+
+// Metadata keys used to pass from gateway to service
+const logLevelMetaKey = "log-level"
+const logFlagMetaKey = "log-trace-key"
+
+// Name of field to be logged if included
+const logFlagFieldName = "log-trace-key"
+
+// Annotator is a function that reads the http headers of incoming requests
+// searching for special logging arguments
+func Annotator(ctx context.Context, req *http.Request) metadata.MD {
+	md := make(metadata.MD)
+	if lvl := req.Header.Get(logLevelHeaderKey); lvl != "" {
+		md[runtime.MetadataPrefix+logLevelMetaKey] = []string{lvl}
+	}
+	if flag := req.Header.Get(logFlagHeaderKey); flag != "" {
+		md[runtime.MetadataPrefix+logFlagMetaKey] = []string{flag}
+	}
+
+	return md
+}
