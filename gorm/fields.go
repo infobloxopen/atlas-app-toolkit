e95f9ddc693bdf94d804314712b2c36b6d022ea7
Ying Chen <ychen@infoblox.com>
2022-07-22 08:33:26 -0700

    update comment

--- gorm/fields.go
+++ gorm/fields.go
@@ -92,7 +92,7 @@ func handlePreloads(f *query.Field, objType reflect.Type) ([]string, error) {
 		}
 
 		for _, subString := range strings.Split(s, "_") {
-			// TODO: if there are multiple substrings, only first one is replaced
+			// TODO: if there are multiple same substrings, only first one is replaced
 			fieldName := strings.Replace(s, subString, strings.ToUpper(subString), 1)
 			fieldName = util.Camel(fieldName)
 			findFieldByName, ok = objType.FieldByName(fieldName)
