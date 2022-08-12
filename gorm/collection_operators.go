e964263a49bfbcf3662fb6798271b88efac63fa0
Ankhit Bala Venkata <abalavenkata@infoblox.com>
2022-08-12 15:44:50 -0700

    More FTS fixes

--- gorm/collection_operators.go
+++ gorm/collection_operators.go
@@ -89,9 +89,13 @@ func ApplySearchingEx(ctx context.Context, db *gorm.DB, s *query.Searching, obj 
 	}
 	if s != nil && s.Query != "" {
 		s.Query = strings.TrimSpace(s.Query)
-		splChar := []string{"::", ":", "(", ")", "|", "+", "<", "'", "&"}
+		s.Query = strings.ReplaceAll(s.Query, ":", " ")
+		splChar := []string{"(", ")", "|", "+", "<", "'", "&", "!", "%", ";"}
 		for _, spl := range splChar {
-			s.Query = strings.ReplaceAll(s.Query, spl, " ")
+			if strings.Contains(s.Query, spl) {
+				s.Query = ""
+				return db.Where(str, s.Query), nil
+			}
 		}
 		s.Query = strings.Join(strings.Fields(s.Query), " ")
 		if s.Query != "" {
