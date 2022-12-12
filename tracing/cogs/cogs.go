64dafbf2336e79bc0b574243f3c6dd8e5a6f5c07
Hamid Ramazani <hramazani@infoblox.com>
2022-12-12 10:08:05 -0800

    add util to tracing to get function name, and further COGS constants

--- tracing/cogs/cogs.go
+++ tracing/cogs/cogs.go
@@ -2,7 +2,10 @@ package cogs
 
 // const values used in setting span attributes for COGS: Cost Of Goods Sold.
 const (
-	CostOfGoodsKey       = "cogs"
-	CostOfGoodsMethodKey = "cogs.method"
-	CostOfGoodsAccountID = "cogs.accountID"
+	CostOfGoodsKey          = "cogs"
+	CostOfGoodsMethodKey    = "cogs.method"
+	CostOfGoodsAccountID    = "cogs.accountID"
+	CostOfGoodsRDS          = "cogs.rds"
+	CostOfGoodS3            = "cogs.s3"
+	CostOfGoodElasticSearch = "cogs.elasticsearch"
 )
